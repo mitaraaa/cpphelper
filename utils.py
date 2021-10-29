f390b2372f4e21edde8affd8b8acfc8fa4bdda47
zhilkaidarov <rustam030201@gmail.com>
2021-10-29 21:41:57 +0600

    callback handlers rework

---
+++ utils.py
@@ -0,0 +1,19 @@
+import pickle
+
+# Read from locales.pickle dictionary of locales
+def read_pickle():
+	data = { }
+	with open("locales.pickle", "rb") as file_r:
+		while True:
+			try:
+				data = pickle.load(file_r)
+			except EOFError:
+				break
+	return data
+
+# Combines all strings from json into one
+def parse_article(article):
+	text = ""
+	for line in article:
+		text += article[line] + "\n"
+	return text
